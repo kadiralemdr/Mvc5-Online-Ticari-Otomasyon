931d47c763bc651f02bb20983ed6a70293ea3988
kadiralemdr <124174355+kadiralemdr@users.noreply.github.com>
2023-07-27 17:29:57 +0300

    Mvc5 Online Ticari Otomasyon
    ASP.NET MVC5 ile Ticari Otomasyon Uygulması (Code First)

---
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Models/Siniflar/Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Class2
+    {
+        [Key]
+        public int id { get; set; }
+        public string deneme { get; set; }
+    }
+}
